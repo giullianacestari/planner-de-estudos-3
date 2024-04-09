151d7b905a4f1ba0841fd1c0c346fde83f887c93
giullianacestari <dev.giullianacestari@gmail.com>
2024-04-09 15:45:09 -0300

    Update main.js

--- assets/main.js
+++ assets/main.js
@@ -22,25 +22,10 @@ function novaTarefa() {
 
 // Criando um id único
 function geraId() {
-  function s4() {
-    return Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
+  function sequenciaAleatoria() {
+    return Math.random().toString(10).substring(2, 6);
   }
-  return (
-    s4() +
-    s4() +
-    "-" +
-    s4() +
-    "-" +
-    s4() +
-    "-" +
-    s4() +
-    "-" +
-    s4() +
-    s4() +
-    s4()
-  );
+  return sequenciaAleatoria();
 }
 
 // Desenhando a tabela com JS
